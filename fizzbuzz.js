9e7dc1fc193c9a0ac13d21848be8c748e2ab1104
viktorx64 <viktor.sundqvist2@gmail.com>
2021-01-11 13:49:03 +0100

    made a test for check if number is dividing with 3

---
+++ fizzbuzz.js
@@ -0,0 +1,24 @@
+console.log("Fizz-buzz");
+
+//check if dividible by 5
+
+function check5(num) {
+	if (num % 3 == 0) {
+		console.log("yes box");
+	} else {
+		console.log("no");
+	}	
+
+}
+
+//check if dividible by 3
+
+
+//loop trough the numbers
+
+var i = 0;
+
+while(i<10) {
+	check5(i);
+	i++;
+}
