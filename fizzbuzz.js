5b7ea675b1af32dd125a6fe8070e3b5a1055bd52
viktorx64 <viktor.sundqvist2@gmail.com>
2021-01-11 14:33:34 +0100

    made the fizz or buzz replace the number

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,28 +3,33 @@ console.log("Fizz-buzz");
 //check if dividible by 5
 
 function check5(num) {
-	if (num % 3 == 0) {
-		console.log("buzz");
-	}
-
+	return (num % 5 == 0);
+	
 }
 
 //check if dividible by 3
 
 function check3(num) {
-	if (num % 5 == 0) {
-		console.log("fizz");
-	}
+	return(num % 3 == 0);
 }
 
 //loop trough the numbers
 
-var i = 0;
+var i = 1;
 
 while(i<100) {
-	console.log(i + "{");
-	check5(i);
-	check3(i);
-	console.log("}");
+	var printer = ""; 
+	var five = check5(i);
+	var three = check3(i);
+	if(three) {
+		printer += "fizz";
+	}
+	if(five) {
+		printer += "buzz";
+	}
+	if(printer == "") {
+		printer = i;
+	}
+	console.log(printer);
 	i++;
 }
