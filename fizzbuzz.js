977aa55727c3b9354054dfcc8fa3e858fa7b1e4f
viktorx64 <viktor.sundqvist2@gmail.com>
2021-01-11 13:54:33 +0100

    made it say fizz and buzz at what seems the right placres

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,21 +4,27 @@ console.log("Fizz-buzz");
 
 function check5(num) {
 	if (num % 3 == 0) {
-		console.log("yes box");
-	} else {
-		console.log("no");
-	}	
+		console.log("buzz");
+	}
 
 }
 
 //check if dividible by 3
 
+function check3(num) {
+	if (num % 5 == 0) {
+		console.log("fizz");
+	}
+}
 
 //loop trough the numbers
 
 var i = 0;
 
-while(i<10) {
+while(i<100) {
+	console.log(i + "{");
 	check5(i);
+	check3(i);
+	console.log("}");
 	i++;
 }
